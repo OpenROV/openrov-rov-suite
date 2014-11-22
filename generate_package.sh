0054950738dc2590e3d3cf0f7184501122460a3d
BrianAdams <brian@babastik.com>
2014-11-21 21:14:16 -0800

    test

--- generate_package.sh
+++ generate_package.sh
@@ -5,17 +5,17 @@ cat >> make_package.sh << __EOF__
 #!/bin/bash
 set -e
 fpm -f -m info@openrov.com -s dir -t deb -a armhf\
- -n openrov-onrov-suite\
+ -n openrov-onrov-suite
 __EOF__
 
 while read package; do
-  echo package | awk '-d {print $1}' >> make_package.sh
+  echo package | awk '-d {print $1}\' >> make_package.sh
 done < manifest
 
 
 cat >> make_package.sh << __EOF__
 -v $VERSION_NUMBER-$REAL_GIT_BRANCH\
---description 'OpenROV suite of projects that run on the ROV directly' .=/opt/openrov\
+--description 'OpenROV suite of projects that run on the ROV directly' .=/opt/openrov
 __EOF__
 
 cat make_package.sh
