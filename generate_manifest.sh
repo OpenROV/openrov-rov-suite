9a1691b01eac0ddf020a0448d25243b5feb8836d
BrianAdams <brian@babastik.com>
2014-11-21 22:09:41 -0800

    test

--- generate_manifest.sh
+++ generate_manifest.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 while read package; do
+  apt-cache madison package
   apt-cache madison package | awk '{print $1,$3}' >> manifest
-done <inventory
+done < inventory
 
 cat manifest
--- generate_package.sh
+++ generate_package.sh
@@ -4,8 +4,9 @@ set -e
 cat >> make_package.sh << __EOF__
 #!/bin/bash
 set -e
-fpm -f -m info@openrov.com -s dir -t deb -a armhf\
- -n openrov-onrov-suite __EOF__
+__EOF__
+
+echo -n "fpm -f -m info@openrov.com -s dir -t deb -a armhf -n openrov-onrov-suite" >> make_package.sh
 
 while read package; do
   echo -n package | awk '-d {print $1}\' >> make_package.sh
