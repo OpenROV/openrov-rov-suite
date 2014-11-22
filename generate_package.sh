0d19023e1812bd729f25c64cc7a4fdff88d5380a
BrianAdams <brian@babastik.com>
2014-11-21 22:00:06 -0800

    test

--- generate_package.sh
+++ generate_package.sh
@@ -5,8 +5,7 @@ cat >> make_package.sh << __EOF__
 #!/bin/bash
 set -e
 fpm -f -m info@openrov.com -s dir -t deb -a armhf\
- -n openrov-onrov-suite
-__EOF__
+ -n openrov-onrov-suite __EOF__
 
 while read package; do
   echo -n package | awk '-d {print $1}\' >> make_package.sh
