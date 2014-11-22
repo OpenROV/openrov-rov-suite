4754faa17174ae491d623dace08bef9593f0ac6e
BrianAdams <brian@babastik.com>
2014-11-22 13:59:23 -0800

    t

--- generate_manifest.sh
+++ generate_manifest.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 while read package; do
-  apt-cache madison $package
-  apt-cache madison $package | awk '{print $1,$3}' >> manifest
+#  apt-cache madison $package
+#  apt-cache madison $package | awk '{print $1,$3}' >> manifest
+  apt-cache policy $package | grep Candidate: | awk '{print $package $2}'
+  apt-cache policy $package | grep Candidate: | awk '{print $package $2}' >> manifest
 done < inventory
 
 cat manifest
