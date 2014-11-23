8fefd56fe2298ec7804d94dc444cc7d2b8e9a88e
BrianAdams <brian@babastik.com>
2014-11-22 22:35:44 -0800

    Version number pulls from package.json

--- generate_manifest.sh
+++ generate_manifest.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 set -e
-while read package; do
-#  apt-cache madison $package
-#  apt-cache madison $package | awk '{print $1,$3}' >> manifest
-  apt-cache policy $package | grep Candidate: | awk -v pkg_name=$package '{print pkg_name, $2}'
-  apt-cache policy $package | grep Candidate: | awk -v pkg_name=$package '{print pkg_name, $2}' >> manifest
-done < inventory
+
+if [ $REAL_GIT_BRANCH -eq master ]
+then
+  while read package; do
+  #  apt-cache madison $package
+  #  apt-cache madison $package | awk '{print $1,$3}' >> manifest
+    apt-cache policy $package | grep Candidate: | awk -v pkg_name=$package '{print pkg_name, $2}'
+    apt-cache policy $package | grep Candidate: | awk -v pkg_name=$package '{print pkg_name, $2}' >> manifest
+  done < inventory
+else
+  # When branching from development, you must also create a new manifest_release to lock the dependency versions
+  cp manifest_release manifest
+fi
 
 cat manifest
--- generate_package.sh
+++ generate_package.sh
@@ -13,7 +13,10 @@ while read package; do
   echo -n $package | awk 'BEGIN{ORS="";} {!seen[$1]++} {print " -d", $1">"$2}' >> make_package.sh
 done < manifest
 
-
+if [ $REAL_GIT_BRANCH -eq stable ]
+echo -n "-v $VERSION_NUMBER --description 'OpenROV suite of projects that run on the ROV directly' .=/opt/openrov" >> make_package.sh
+else
 echo -n "-v $VERSION_NUMBER-$REAL_GIT_BRANCH --description 'OpenROV suite of projects that run on the ROV directly' .=/opt/openrov" >> make_package.sh
+fi
 
 cat make_package.sh
