769513e9f593fed5dfb202b16c0eb8e2a3145ce9
BrianAdams <brian@babastik.com>
2014-11-23 00:20:00 -0800

    .

--- generate_package.sh
+++ generate_package.sh
@@ -4,6 +4,7 @@ set -e
 cat >> make_package.sh << __EOF__
 #!/bin/bash
 set -e
+set -vx
 __EOF__
 
 echo -n "fpm -f -m info@openrov.com -s dir -t deb -a armhf -n openrov-onrov-suite" >> make_package.sh
@@ -20,6 +21,4 @@ else
   echo -n "-v $VERSION_NUMBER-$REAL_GIT_BRANCH --description 'OpenROV suite of projects that run on the ROV directly' .=/opt/openrov" >> make_package.sh
 fi
 
-while read line; do
-  echo $line
-done < make_package.sh
+cat make_package.sh
