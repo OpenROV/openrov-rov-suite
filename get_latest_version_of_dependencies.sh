666beb67c75650bfca66733aa55053ebebf4f01b
BrianAdams <brian@babastik.com>
2015-04-07 00:53:43 -0700

    Added deploy scripts

---
+++ get_latest_version_of_dependencies.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#for each line in the nightly-repos folder
+#get the latest file from the repo
+#publish those files to the debian repo
---
+++ publish_to_debian_repo.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#docker run -t --rm -v $(pwd):/work -w /work smashwilson/curl /bin/bash publish_to_debian_repo.sh openrov-rov-suite_30.0.0~~pre-release.70.d71e4da_armhf.deb pre-release
+set -e
+
+
+BASEURL=http://openrov-software-nightlies.s3-website-us-west-2.amazonaws.com/
+IFS=$'\n'
+BRANCH=$2
+JENKINS_TOKEN_NAME=vd2sf9fk84mKm4G
+SUITEURL=${BASEURL}openrov-rov-suite/$1
+
+#TEMPDIR=`mktemp -d` && pushd $TEMPDIR
+curl -O $SUITEURL
+#get the list of dependent files
+for ITEM in `dpkg -I $1 | grep Depends: | sed 's/ (=/_/g' | sed 's/)/_armhf.deb/g' | sed 's/Depends: //g' | sed 's/ //g' | tr ',' '\n'`
+do
+  RAWNAME=$(echo $ITEM | awk -F '[_]' '{print $1}')
+  URL=$(cat nightly-repos | grep $RAWNAME | awk -F '[  ]' '{print $2}'|sed 's/?prefix=//' )
+  DEBURL=${URL}${ITEM}
+
+  echo "publishing $ITEM"
+  curl -g http://openrov-build-test.elasticbeanstalk.com:8080/job/OpenROV-generic-upload-deb-to-repo/buildWithParameters -d token=$JENKINS_TOKEN_NAME -d urlToDebPackage=$DEBURL -d branch=$BRANCH
+done
+curl -g http://openrov-build-test.elasticbeanstalk.com:8080/job/OpenROV-generic-upload-deb-to-repo/buildWithParameters -d token=$JENKINS_TOKEN_NAME -d urlToDebPackage=$SUITEURL -d branch=$BRANCH
+echo "publishing $1"
+
+#popd
+#rm -rf $TEMPDIR
