22180b786e66beb595677d4f47aa684bda37ab64
BrianAdams <brian@babastik.com>
2015-06-20 17:17:23 -0700

    test

--- generate_manifest.sh
+++ generate_manifest.sh
@@ -26,7 +26,7 @@ then
   cat ${TEMPDIR}/latest_files.txt
   #get the list of dependent files
   while read item; do
-    echo $item | awk -F'[_/]' '{print $(NF-1) " " $NF}' >> manifest
+    echo $item | awk -F'[_/]' '{print $(NF-2) " " $(NF-1)}' >> manifest
   done < ${TEMPDIR}/latest_files.txt
 
   rm -rf $TEMPDIR
